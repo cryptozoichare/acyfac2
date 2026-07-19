533e1b457164b49d454e5e8a56388ae03218a36b
Acy Factory <jruskowitz@gmail.com>
2026-07-18 17:26:18 -0700

    static

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,6 +128,8 @@ LANGUAGES = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
